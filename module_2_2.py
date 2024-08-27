4a8654da431225260c78b4c5228e2c4ff8fcbe8b
Yarik <yarik.slon@gmail.com>
2024-08-27 20:19:16 +0300

    initial

---
+++ module_2_2.py
@@ -0,0 +1,7 @@
+first, second, third = int(input('Введите первое число')), int(input('Введите второе число')), int(input('Введите последнее число'))
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print()
