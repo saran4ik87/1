dbc5065cdad9b2ab8b2193783d6152afef64c8fb
Yarik <yarik.slon@gmail.com>
2024-08-30 13:39:38 +0300

    initial

--- module_2_3.py
+++ module_2_3.py
@@ -1,5 +1,6 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 n = 0
 while n in range(len(my_list)) and my_list[n] >= 0:
-    print(my_list[n])
+    if my_list[n] > 0:
+        print(my_list[n])
     n += 1
