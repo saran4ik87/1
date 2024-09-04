ea99be6ead0a1a40f45a796991bf144a938c23ee
Yarik <yarik.slon@gmail.com>
2024-09-04 19:48:37 +0500

    initial

--- module_3_2.py
+++ module_3_2.py
@@ -1,4 +1,4 @@
-def send_email(message, recipient, sender = "university.help@gmail.com"):
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
     if ("@" not in recipient and sender or not sender.lower().endswith(tuple([".com", ".ru", ".net"])) or not
             recipient.lower().endswith(tuple([".com", ".ru", ".net"]))):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
