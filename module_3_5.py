4dc497b5cc18a33a69ae83248a22fb165d9aea39
Yarik <yarik.slon@gmail.com>
2024-09-07 21:50:09 +0500

    initial

---
+++ module_3_5.py
@@ -0,0 +1,23 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+
+result = get_multiplied_digits(8555)
+print(result)
+
+result = get_multiplied_digits(40203)
+print(result)
+
+result = get_multiplied_digits(123)
+print(result)
+
+result = get_multiplied_digits(1119)
+print(result)
+
+result = get_multiplied_digits(25)
+print(result)
