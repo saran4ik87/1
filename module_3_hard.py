9757945a882174ac9c64edb3cf3f41201c95b7b6
Yarik <yarik.slon@gmail.com>
2024-09-11 17:14:25 +0500

    initial

---
+++ module_3_hard.py
@@ -0,0 +1,26 @@
+def calculate_structure_sum(data):
+    sum = 0
+    if isinstance(data, dict):
+        dict_data = list(data.items())
+    else:
+        dict_data = data
+    for d in dict_data:
+        if isinstance(d, int):
+            sum += d
+        elif isinstance(d, str):
+            sum += len(d)
+        else:
+            sum += calculate_structure_sum(d)
+    return sum
+
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+    ]
+
+result = calculate_structure_sum(data_structure)
+print(result)
